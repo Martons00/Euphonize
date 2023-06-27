410aef55f9d225aa82382a2a704bba6fca62c079
Martons00 <raffaelemartone34@gmail.com>
2023-06-27 17:48:08 +0200

    Initial Commit

---
+++ EuphonizeProject/EuphonizeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuphonizeProjectApp.swift
+//  EuphonizeProject
+//
+//  Created by Raffaele Martone on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EuphonizeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
